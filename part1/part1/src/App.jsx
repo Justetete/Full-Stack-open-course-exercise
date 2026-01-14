4cfc9360d8ca0da0408f3a93d2ff7b3f6bd8996f
Xinchi Jian <justeriam@gmail.com>
2026-01-14 11:24:18 +0000

    update part1 exercise

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -2,26 +2,57 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react'
 
-const Hello = (props) => {
-  console.log(props)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
-  const age = 28
-  const name = 'Peter'
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState([])
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    console.log('left before', left)
+    const updateLeft = left + 1
+    setLeft(updateLeft)
+    console.log('left after', updateLeft)
+    setTotal(right + updateLeft)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updateRight = right + 1
+    setRight(updateRight)
+    setTotal(updateRight + left)
+  }
+
+
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='George' age={26+10} />
-      <Hello name={name} age={age} />
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
     </div>
   )
 }
