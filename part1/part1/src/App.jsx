316743017d764f3226ac0c1cf00246760837112e
Xinchi Jian <justeriam@gmail.com>
2026-01-15 17:20:45 +0000

    update e1.6

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -3,58 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+const Display = props => <div>{props.value}</div>
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState([])
+  const [value, setValue] = useState(10)
 
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    console.log('left before', left)
-    const updateLeft = left + 1
-    setLeft(updateLeft)
-    console.log('left after', updateLeft)
-    setTotal(right + updateLeft)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updateRight = right + 1
-    setRight(updateRight)
-    setTotal(updateRight + left)
-  }
-
-
+  
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
-      <p>total {total}</p>
+      <Display value = {value}/>
+
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
+
+  
 
 export default App
